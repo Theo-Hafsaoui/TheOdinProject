c973ef3901f4c6e7ba86c420fb11870c8478524c
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2022-07-04 17:56:20 +0200

    working version, but issue with random

--- RockPaperScissors/src/game.js
+++ RockPaperScissors/src/game.js
@@ -1,7 +1,40 @@
+
+var player_score = 0;
+var comput_score = 0;
+
+var fire = document.querySelector('input.Fire');
+var water = document.querySelector('input.Water');
+var grass = document.querySelector('input.Grass');
+
+var score = document.querySelector('h1#sc');
+var top_text = document.querySelector('h1#top');
+
+fire.addEventListener('click', pick_fire);
+water.addEventListener('click', pick_water);
+grass.addEventListener('click', pick_grass);
+
+function pick_fire() {
+  com = computerPlay();
+  round('fire', com)
+  console.log("test");
+}
+
+function pick_water() {
+  com = computerPlay();
+  round('water', com)
+  console.log("test");
+}
+
+function pick_grass() {
+  com = computerPlay();
+  round('grass', com)
+  console.log("test");
+}
+
 function computerPlay() {
   //Function that randomly
   //return a string from l
-  const l=["rock","paper","scissor"];
+  const l=["water","fire","grass"];
   rn=math.random()*3
   return l[math.round(rn)%3];
 }
@@ -9,53 +42,41 @@ function computerPlay() {
 function round(player,computer) {
   //Return the result of the match
   player=player.toLowerCase()
+  console.log(player,computer)
   computer=computer.toLowerCase()
   if (player == computer) {
-    return("--Draw--")
+    top_text.textContent = ("--Draw--")
   }
-  if (player == "rock") {
-    if (computer== "paper") {
-     return("You Lose! Paper beats Rock") 
+  else if (player == "fire") {
+    if (computer== "water") {
+     top_text.textContent = ("You Lose! Water beats Fire") 
+     comput_score++;
     }
     else{
-     return("You Win! Rock beats scissor") 
+     top_text.textContent = ("You Win! Fire beats grass") 
+     player_score++;
     }
   }
-  if (player == "paper") {
-    if (computer== "rock") {
-     return("You Win! Paper beats Rock") 
+  else if (player == "water") {
+    if (computer== "fire") {
+     top_text.textContent = ("You Win! Water beats Fire") 
+     player_score++;
     }
     else{
-     return("You lose! Paper beats Rock") 
+     top_text.textContent = ("You lose! Water beats Fire") 
+     comput_score++;
     }
   }
-  if (player == "scissor") {
-    if (computer== "rock") {
-     return("You lose! Rock beats Scissor") 
+  else if (player == "grass") {
+    if (computer== "fire") {
+     top_text.textContent = ("You lose! Fire beats Grass") 
+     comput_score++;
     }
     else{
-     return("You Win! Scissor beats Paper") 
+     top_text.textContent = ("You Win! Grass beats Water") 
+     player_score++;
     }
   }
+  score.textContent = ("score:"+player_score+"-"+comput_score);
 }
 
-
-function game() {
-  continu= 1;
-  const l=["rock","paper","scissor"];
-  while (continu) {
-    let choice = prompt("0:rock\n1:paper\n2:scissor");
-    if (Number(choice)>2) {
-     continu=NaN; 
-     break;
-    }
-    let player_choice=l[Number(choice)];
-    let computer_choice=computerPlay()
-    console.log(round(player_choice,
-      computer_choice))
-  }
-  return 0;
-}
-
-//MAIN
-//game();
