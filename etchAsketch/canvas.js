619668ded877d9600d6a30ba29ed281cfc9a8cc1
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2022-07-06 16:53:50 +0200

    Add clear button

--- etchAsketch/canvas.js
+++ etchAsketch/canvas.js
@@ -10,15 +10,29 @@ function init_cnv(s) {
     for (let _ = 0; _ < s; _++) {
        var block = document.createElement('div');
        block.classList.add('block')
-      block.addEventListener("mouseenter", function(block) {
+       block.addEventListener("mouseenter", function(block) {
         block.target.style.backgroundColor = "purple";
-      }, false);
+       }, false);
        ligne.appendChild(block);
     }
     cnv.appendChild(ligne);
   }
 }
 
+const clear_button = document.querySelector('input#clear');
+clear_button.addEventListener("click", reset_cnv);
+
+function reset_cnv(){
+  for (let i = 0; i < cnv.childNodes.length; i++) {
+    const ligne = cnv.childNodes[i];
+    for (let j = 0; j < ligne.childNodes.length; j++) {
+      const block = ligne.childNodes[j];
+      block.style.backgroundColor = "white";
+    }
+  }
+}
+
 //Main
 init_cnv(16);
+
 
